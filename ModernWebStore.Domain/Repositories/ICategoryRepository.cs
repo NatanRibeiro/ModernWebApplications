88e7012147210cf6de7a0fdb7330c30c040aff77
nDutra <natan@DESKTOP-OMT51LO>
2016-12-19 23:03:58 -0200

    Was added the new itens as Repositores and Services. Repositories: Will persist and read informations from the database. Services: Application Services, will orchestrate the requisitions at domain

---
+++ ModernWebStore.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using ModernWebStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace ModernWebStore.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        List<Category> Get();
+        List<Category> Get(int skip, int take);
+        Category Get(int id);
+        void Create(Category cateogry);
+        void Update(Category cateogry);
+        void Delete(Category cateogry);
+    }
+}
---
+++ ModernWebStore.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ModernWebStore.Domain.Entities;
+
+namespace ModernWebStore.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        void Register(User user);
+        User Authenticate(string email, string password);
+        User GetByEmail(string email);
+    }
+}
