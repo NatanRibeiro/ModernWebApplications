0205a6ca7c7d97e27dbeb0a66377f6f70bdf9be7
nDutra <natan@DESKTOP-OMT51LO>
2016-12-22 23:47:07 -0200

    Was created a project to presentation called Api

---
+++ ModernWebStore.Api/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ModernWebStore.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            //var container = new UnityContainer();
+
+            //ConfigureDependencyInjection(config, container);
+            //ConfigureWebApi(config);
+
+            //ConfigureOAuth(app, container.Resolve<IUserApplicationService>());
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ ModernWebStore.CrossCutting/DependencyRegister.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Practices.Unity;
+using ModernWebStore.ApplicationService;
+using ModernWebStore.Domain.Repositories;
+using ModernWebStore.Domain.Services;
+using ModernWebStore.Infrastructure.Persistence;
+using ModernWebStore.Infrastructure.Persistence.DataContext;
+using ModernWebStore.Infrastructure.Repositories;
+using ModernWebStore.SharedKernel;
+using ModernWebStore.SharedKernel.Events;
+
+namespace ModernWebStore.CrossCutting
+{
+    public static class DependencyRegister
+    {
+        /// <summary>
+        /// TransientLifetimeManager - Cada Resolve gera uma nova instancia.
+        /// HierarchicalLifetimeManager - Utiliza Singleton
+        /// </summary>
+        /// <param name="container"></param>
+        public static void Register(UnityContainer container)
+        {
+            container.RegisterType<StoreDataContext, StoreDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<ICategoryRepository, CategoryRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IProductRepository, ProductRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IOrderRepository, OrderRepository>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<IUserApplicationService, UserApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<ICategoryApplicationService, CategoryApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IProductApplicationService, ProductApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IOrderApplicationService, OrderApplicationService>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<IHandler<DomainNotification>, DomainNotificationHandler>(new HierarchicalLifetimeManager());
+        }
+    }
+}
