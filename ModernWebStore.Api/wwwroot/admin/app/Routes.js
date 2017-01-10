8f311ff88f0c888bdd986d291e1069d9a1b5eb2e
nDutra <natan@DESKTOP-OMT51LO>
2017-01-10 01:21:02 -0200

    Was initialized the angular implementation

---
+++ ModernWebStore.Api/wwwroot/admin/app/Routes.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mwa').config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeCtrl',
+                controllerAs: 'vm',
+                templateUrl: 'pages/home/index.html'
+            });
+    });
+})();
---
+++ ModernWebStore.Api/wwwroot/admin/app/controllers/home/home-controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mwa').controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = [];
+
+    function HomeCtrl($scope) {
+        var vm = this;
+
+        vm.activate = activate;
+
+        activate();
+
+        function activate() {
+
+        }
+    }
+})();
